1705edcce679e771f168e8988f0617dd492b5dc3
victoria <velms18@gmail.com>
2024-08-14 14:59:43 -0300

    OAuth2 start, login page redirecting

--- src/main/java/com/cst8277/Elms/Victoria/TwitterLikeApp.java
+++ src/main/java/com/cst8277/Elms/Victoria/TwitterLikeApp.java
@@ -2,13 +2,17 @@ package com.cst8277.Elms.Victoria;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
@@ -17,38 +21,41 @@ import java.util.Map;
 @SpringBootApplication
 //@ComponentScan(basePackages="com.cst8277.Elms.Victoria")
 @RestController
-class TwitterLikeApp {//extends WebSecurityConfigurerAdapter{
-
-    @GetMapping("/user")
-    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal){
-        return Collections.singletonMap("user", principal.getAttribute("user"));
-
-    }
+class TwitterLikeApp {
 
     public static void main(String[] args){
         SpringApplication.run(TwitterLikeApp.class, args);
-
-
-
     }
 
-   /*@Override
-    protected void configure(HttpSecurity http) throws Exception{
-        http
-                .authorizeRequests(a -> a
-                        .antMatchers("/"), "/error", "/webjars/**").permitAll()
-                .anyRequest().authenticated()
-                .logout(l -> l
-                        .logoutSuccessUrl("/").permitAll()
-                )
-        .exceptionHandling(e -> e
-                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
-                .oauth2Login();
-
-
+   /* @GetMapping("/user")
+    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal){
+        return Collections.singletonMap("user", principal.getAttribute("user"));
 
     }*/
+    @Controller
+    public class WebController {
+        @RequestMapping("/")
+        public String index(){
+            return "index";
+        }
+    }
 
-
-
+  /*  @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .authorizeHttpRequests((authorizeRequests -> authorizeRequests
+                        .requestMatchers("/", "/error", "/webjars/**").permitAll()
+                        .anyRequest().authenticated()
+                ))
+                .exceptionHandling(exceptionHandling -> exceptionHandling
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                )
+                .oauth2Login(oauth2Login -> oauth2Login.defaultSuccessUrl("/", true))
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/")
+                        .invalidateHttpSession(true)
+                        .deleteCookies("JSESSIONID"));
+        return http.build();
+    }*/
 }
